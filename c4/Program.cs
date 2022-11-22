66bfa62a259dbc88ad55ecee331a0882dc41e616
LadaSPB <LadaSpb@yandex.ru>
2022-11-22 05:01:42 +0300

    задача10

---
+++ c4/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.Write("Введите трехзначное число: ");
+int Numero = Convert.ToInt32(Console.ReadLine());
+string SecondNumero = Convert.ToString(Numero);
+Console.WriteLine("Вторая цифра этого числа: "+SecondNumero[1]);
+
