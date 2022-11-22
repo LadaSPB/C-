23d836d4876e9c1dd91086c148e7d23e4fd34be6
LadaSPB <LadaSpb@yandex.ru>
2022-11-22 07:15:42 +0300

    задача15

---
+++ c6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите день недели: ");
+int dayofweek = int.Parse (Console.ReadLine());
+if (dayofweek == 6 || dayofweek == 7) 
+{
+    Console.WriteLine ("Выходной день");
+}
+else if (dayofweek <= 5) 
+{
+ Console.WriteLine ("Будний день");
+}
+else if (dayofweek > 7) 
+{
+    Console.WriteLine ("В неделе 7 дней");
+}		
