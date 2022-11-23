a5504a6a08a33600e064f2a6389c468e212155f1
LadaSPB <LadaSpb@yandex.ru>
2022-11-23 14:53:42 +0300

    задача19

---
+++ c7/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+string writeTxt = Console.ReadLine();
+string txtToCheck = writeTxt.ToLower().Replace(" ","");
+int length = txtToCheck.Length;
+string result = "не является";
+
+bool IsPalindrome(string p)
+{
+    int count = 0;
+    while (count <= length)
+    {
+        if (txtToCheck[count] != txtToCheck[length - 1]) return false;
+        length--;
+        count++;
+    }
+    return true;
+}
+
+if (IsPalindrome(txtToCheck)) result = "является";
+Console.WriteLine($"Введенное значение {result} палиндромом");
+Console.WriteLine();
