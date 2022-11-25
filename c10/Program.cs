06b5468b3b60404964a143b79870f73e96f90115
LadaSPB <LadaSpb@yandex.ru>
2022-11-25 21:28:09 +0300

    задача 25

---
+++ c10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+int exponentiation = Exponentiation(numA, numB);
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"{numA} в степени {numB} = {exponentiation}");
+Console.ResetColor();
