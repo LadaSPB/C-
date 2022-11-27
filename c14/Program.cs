8fb8cb375f596b4f06477b2ba81b07699c1c3c2f
LadaSPB <LadaSpb@yandex.ru>
2022-11-27 04:40:45 +0300

    задача 36

---
+++ c14/Program.cs
@@ -0,0 +1,34 @@
+﻿int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sumNumbersEvenIndex = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sumNumbersEvenIndex += numbers[i];
+}
+Console.Write(sumNumbersEvenIndex);
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+ 
+}
+
+Console.ResetColor();
+ 
