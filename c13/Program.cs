d94d13ab176e3073a10e17607cffb33714de7593
LadaSPB <LadaSpb@yandex.ru>
2022-11-27 04:08:20 +0300

    задача 34

---
+++ c13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
+}
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
+Console.ResetColor();
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
