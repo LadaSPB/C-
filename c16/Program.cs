9c82c86b4fc8c2dbd051f5b7077b0abd56a3ab8e
LadaSPB <LadaSpb@yandex.ru>
2022-11-29 17:14:01 +0300

    задача 41

---
+++ c16/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Введите числа через запятую: ");
+string? input = Console.ReadLine();
+int[] numbers = ParseStringToArray(input);
+PrintArray(numbers);
+
+ 
+int Comparison(int[] Numbers)
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if(Numbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+ 
+Console.WriteLine($"Введено чисел больше нуля >> {Comparison(numbers)} ");
+
+int[] ParseStringToArray(string input)
+{
+    int countNumbers = GetCountNumbersInString(input);
+    int[] numbers = new int[countNumbers];
+    int indexNumber = 0;
+    string substring = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ',')
+        {
+            substring += input[i];
+        }
+        else
+        {
+            numbers[indexNumber] = Convert.ToInt32(substring);
+            indexNumber++;
+            substring = string.Empty;
+        }
+    }
+    if(input[input.Length - 1] != ',')
+        numbers[indexNumber] = Convert.ToInt32(substring);
+    return numbers;
+}
+int GetCountNumbersInString(string input) 
+{
+    int count;
+    if(input[input.Length - 1] == ',')
+        count = 0;
+    else
+        count = 1;
+
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i] == ',')
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
