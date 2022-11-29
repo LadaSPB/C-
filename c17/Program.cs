f743e31fadfcd62a0ad21cfdc505f5c00a93b606
LadaSPB <LadaSpb@yandex.ru>
2022-11-29 17:59:51 +0300

    задача 43

---
+++ c17/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("введите значение b1:  ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k1:  ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите значение b2:  ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k2:  ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double numberX = (b2 - b1)/(k1 - k2);
+double numberY = k2 * numberX + b2;
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Точка пересечения двух прямых: ({numberX};{numberY})");
+Console.ResetColor();
+
