c7649e1f333acc56a01113a9c9e24a7efe553d71
LadaSPB <LadaSpb@yandex.ru>
2022-11-30 22:51:12 +0300

    задача 52

---
+++ c20/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Write("Введите количество строк: ");
+int lengthRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int lengthCol = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[lengthRow, lengthCol];
+
+void generate2DArray(int[,] array)
+{
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+
+void print2dArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t",ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i+"\t",ConsoleColor.DarkYellow);
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+generate2DArray(numbers);
+print2dArray(numbers);
+Console.WriteLine();
+
+for (int j = 0; j < lengthCol; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < lengthRow; i++)
+    {
+        sum = (sum + numbers[i, j]);
+    }
+    sum = sum / lengthRow;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Среднее арифметическое {j} столбца: [{Math.Round(sum, 2)} ; {Math.Round(sum, 2)}]");
+    Console.ResetColor();
+}
