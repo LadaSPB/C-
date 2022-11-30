3058bd35780601a8551b4dae96889778775a2166
LadaSPB <LadaSpb@yandex.ru>
2022-11-30 17:03:28 +0300

    задача 47

---
+++ c18/Program.cs
@@ -0,0 +1,39 @@
+﻿int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+        Console.ResetColor();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+    
+}
