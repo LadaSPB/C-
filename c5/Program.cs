68e72aed1d693c365abfb13b687763eb3a09992b
LadaSPB <LadaSpb@yandex.ru>
2022-12-01 23:46:58 +0300

    задача 13обнов

--- c5/Program.cs
+++ c5/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите число: ");
 int AnyNumero = Convert.ToInt32(Console.ReadLine());
-string anyNumeroText = Convert.ToString(AnyNumero);
-if (anyNumeroText.Length > 2)
+string AnyNumeroText = Convert.ToString(AnyNumero);
+if (AnyNumeroText.Length > 2)
 {
-Console.WriteLine("Третья цифра: " + anyNumeroText[2]);
+Console.WriteLine("Третья цифра: " + AnyNumeroText[2]);
 }
 else
 {
