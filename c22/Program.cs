fc35e58ee3b49daf86101883c5380bbfdfd19ca8
LadaSPB <LadaSpb@yandex.ru>
2022-12-02 00:13:19 +0300

    задача 56

---
+++ c22/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] table = new int[4, 4];
+FillArrayRandom(table);
+PrintArray(table);
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Номер строки с наименьшей суммой элементов: ");
+NumberRowMinSumElements(table);
+
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < table.GetLength(1); i++)
+    {
+        minRow += table[0, i];
+    }
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++) sumRow += table[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.ResetColor();
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
