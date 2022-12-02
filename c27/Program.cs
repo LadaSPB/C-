18cdf98133d2a627360206d6494c4020db860da1
LadaSPB <LadaSpb@yandex.ru>
2022-12-03 01:46:19 +0300

    задача 66

---
+++ c27/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write("Cумма натуральных элементов в промежутке: " ); 
+    Console.Write(SumMN(m - 1, n));
+    Console.ResetColor();
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
