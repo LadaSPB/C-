f7b1045978ce2105d711ead388ee9f14ba9c5e37
LadaSPB <LadaSpb@yandex.ru>
2022-12-03 01:24:23 +0300

    задача 64

---
+++ c26/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+
